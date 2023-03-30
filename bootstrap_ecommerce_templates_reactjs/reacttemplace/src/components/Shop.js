de197734979e940b87f8a39cf3395398c52b2c67
Long Hoang <longhoang12212@gmail.com>
2023-03-30 07:38:52 +0700

    Long fix conflict files App.js va Shop.js

--- bootstrap_ecommerce_templates_reactjs/reacttemplace/src/components/Shop.js
+++ bootstrap_ecommerce_templates_reactjs/reacttemplace/src/components/Shop.js
@@ -112,21 +112,14 @@ export default function Shop() {
                         placeholder="Search"
                         onChange={handleOnChangeSearch}
                       ></input>
-<<<<<<< HEAD
+
                       <button
-                        id="searchIconBackGround"
+                        id="searchIconBackground"
                         className="rounded-right"
                       >
-=======
-                      <button id="searchIconBackground" className="rounded-right">
->>>>>>> 9b15ebfba98102857153a467dd2acb66ec21457a
                         <i
                           id="searchIcon"
                           class="tf-ion-android-search"
-<<<<<<< HEAD
-=======
-                          id="searchIcon"
->>>>>>> 9b15ebfba98102857153a467dd2acb66ec21457a
                           onClick={handleSubmit}
                         ></i>
                       </button>
@@ -170,56 +163,52 @@ export default function Shop() {
               </div>
             </div>
 
-
-                    <div className="heading d-flex justify-content-between mb-5">
-                      <span>
-                        <input
-                          id="searchBox"
-                          className="rounded-left pl-3"
-                          placeholder="Search"
-                        ></input>
-                        <button id="searchIconBackground" className="rounded-right">
-                          <i
-                            id="searchIcon"
-                            class="tf-ion-android-search"
-                          ></i>
-                        </button>
-                      </span>
-                      <form className="ordering " method="get">
-                        <select
-                          name="orderby"
-                          className="orderby form-control"
-                          aria-label="Shop order"
-                        >
-                          <option value="" selected="selected">
-                            Sort by size
-                          </option>
-                          <option value="">L Large</option>
-                          <option value="">XL Extra Large</option>
-                          <option value="">M Medium</option>
-                          <option value="">S Small</option>
-                          <option value="">XS Extra Small</option>
-                        </select>
-                        <input type="hidden" name="paged" value="1" />
-                      </form>
-                      <form className="ordering " method="get">
-                        <select
-                          name="orderby"
-                          className="orderby form-control"
-                          aria-label="Shop order"
-                        >
-                          <option value="" selected="selected">
-                            Sort by price
-                          </option>
-                          <option value="">Sort by popularity</option>
-                          <option value="">Sort by average rating</option>
-                          <option value="">Sort by latest</option>
-                          <option value="">Sort by price: low to high</option>
-                          <option value="">Sort by price: high to low</option>
-                        </select>
-                        <input type="hidden" name="paged" value="1" />
-                      </form>
-                    </div>
+            <div className="heading d-flex justify-content-between mb-5">
+              <span>
+                <input
+                  id="searchBox"
+                  className="rounded-left pl-3"
+                  placeholder="Search"
+                ></input>
+                <button id="searchIconBackground" className="rounded-right">
+                  <i id="searchIcon" class="tf-ion-android-search"></i>
+                </button>
+              </span>
+              <form className="ordering " method="get">
+                <select
+                  name="orderby"
+                  className="orderby form-control"
+                  aria-label="Shop order"
+                >
+                  <option value="" selected="selected">
+                    Sort by size
+                  </option>
+                  <option value="">L Large</option>
+                  <option value="">XL Extra Large</option>
+                  <option value="">M Medium</option>
+                  <option value="">S Small</option>
+                  <option value="">XS Extra Small</option>
+                </select>
+                <input type="hidden" name="paged" value="1" />
+              </form>
+              <form className="ordering " method="get">
+                <select
+                  name="orderby"
+                  className="orderby form-control"
+                  aria-label="Shop order"
+                >
+                  <option value="" selected="selected">
+                    Sort by price
+                  </option>
+                  <option value="">Sort by popularity</option>
+                  <option value="">Sort by average rating</option>
+                  <option value="">Sort by latest</option>
+                  <option value="">Sort by price: low to high</option>
+                  <option value="">Sort by price: high to low</option>
+                </select>
+                <input type="hidden" name="paged" value="1" />
+              </form>
+            </div>
 
             <div className="row">
               <div className="col-lg-3 col-12 col-md-6 col-sm-6 mb-5">
@@ -249,7 +238,6 @@ export default function Shop() {
                       <a href="/product-single">Floral Kirby</a>
                     </h2>
                     <span className="price">$329.10</span>
-
                   </div>
                 </div>
               </div>
